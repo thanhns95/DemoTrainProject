82d7379c2f450cb86a5bb3ce286475534051b92e
thanhns <thanhns@ttc-solutions.com>
2020-10-02 14:20:39 +0700

    Initial Commit

---
+++ DemoTrain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoTrain
+//
+//  Created by Nguyễn Thành on 10/2/20.
+//  Copyright © 2020 TrungNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
